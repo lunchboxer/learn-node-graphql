28698a0842e1ff3676d4e5af0079f566c441a28d
James <mrsmith@aliyun.com>
2023-01-21 14:43:27 +0800

    Update index.js

--- src/http/post-graphql/index.js
+++ src/http/post-graphql/index.js
@@ -2,4 +2,5 @@ let arc = require('@architect/functions')
 let query = require('./middleware/query')
 let auth = require('./middleware/auth')
 
+console.log(process.version)
 exports.handler = arc.http.async(auth, query)
